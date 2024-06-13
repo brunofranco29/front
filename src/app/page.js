5b1d621aa0caf932445b56feb21c9bb48782e8aa
brunofranco29 <brunodzie@gmail.com>
2024-06-12 22:30:55 -0300

    front

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,480 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+// "use client"
+// import {useState} from 'react';
+// import styles from './page.module.css'
+// import axios from 'axios'
+
+// export default function Home() {
+//   const [insert, setInsert] = useState(false);
+
+//   function handlerSubmit(event) {
+//     event.preventDefault();
+
+//     const data = new FormData(event.target)
+
+//     const email = data.get('email')
+//     const password = data.get('password')
+//     const nameProduct = data.get('nameProduct')
+//     const description = data.get('description')
+//     const company = data.get('company')
+//     const price = data.get('price')
+//     const amount = data.get('amount')
+
+//     axios.get(`http://localhost:3005/auth/login?email=${email}&password=${password}`)
+//     .then(e => {
+//       const token = e.data.token;
+
+//       axios.post(
+//         `http://localhost:3005/products`,
+//         {
+//           name: nameProduct,
+//           description,
+//           company,
+//           price,
+//           amount
+//         },
+//         {
+//           headers: {
+//             authorization:token
+//           }
+//         }
+//       ).then(element => {
+//         if (element && element.data && element.data.message === 'Product created') {
+//           setInsert(true)
+//         }
+//       })
+//     })
+//   }
+
+//   function formProcess() {
+//     if (insert) {
+//       return (
+//         <main>
+//           <span>Inserção ocorreu com sucesso</span>
+//         </main>
+//       )
+//     }
+
+//     return (
+//       <main>
+//         <h1>Produtos</h1><form onSubmit={handlerSubmit}>
+
+//         <div className={styles.colum}>
+
+//           <div>
+//             <label>
+//               E-mail
+//             </label>
+//           </div>
+//           <div>
+//             <input type="email" name="email"></input>
+//           </div>
+
+//           <div>
+//             <label>
+//               Senha
+//             </label>
+//           </div>
+//           <div>
+//             <input type="password" name="password"></input>
+//           </div>
+
+//         </div>
+
+//         <div className={styles.colum}>
+
+//           <div>
+//             <div>
+//               <label>Nome do Produto</label>
+//             </div>
+//             <div>
+//               <input type="text" name="nameProduct" />
+//             </div>
+//           </div>
+
+//           <div>
+//             <div>
+//               <label>Descrição</label>
+//             </div>
+//             <div>
+//               <input type="text" name="description" />
+//             </div>
+//           </div>
+
+//           <div>
+//             <div>
+//               <label>Empresa</label>
+//             </div>
+//             <div>
+//               <input type="text" name="company" />
+//             </div>
+//           </div>
+
+
+//           <div>
+//             <div>
+//               <label>Preço</label>
+//             </div>
+//             <div>
+//               <input type="number" name="price" />
+//             </div>
+//           </div>
+
+
+//           <div>
+//             <div>
+//               <label>Quantidade</label>
+//             </div>
+//             <div>
+//               <input type="number" name="amount" />
+//             </div>
+//           </div>
+//         </div>
+
+//         <div className={styles.lineButton}>
+//           <button>Login</button>
+//         </div>
+
+//       </form>
+//       </main>
+//     );
+//   }
+
+//   return (
+//     <main>
+//       {formProcess()}
+//     </main>
+//   )
+// }
+//  "use client"
+//  import React, { useState } from 'react';
+//  import styles from './page.module.css';
+//  import axios from 'axios';
+ 
+//  export default function Home() {
+//    const [formData, setFormData] = useState({
+//      email: '',
+//      password: '',
+//      nameProduct: '',
+//      description: '',
+//      company: '',
+//      price: '',
+//      amount: ''
+//    });
+ 
+//    const [insert, setInsert] = useState(false);
+//    const [errorMessage, setErrorMessage] = useState('');
+//    const [notFoundMessage, setNotFoundMessage] = useState('');
+ 
+//    const handleChange = (event) => {
+//      const { name, value } = event.target;
+//      setFormData({ ...formData, [name]: value });
+//    };
+ 
+//    const handleSubmit = async (event) => {
+//      event.preventDefault();
+ 
+//      try {
+//        const loginResponse = await axios.get(`http://localhost:3005/auth/login`, {
+//          params: {
+//            email: formData.email,
+//            password: formData.password
+//          }
+//        });
+ 
+//        const token = loginResponse.data.token;
+ 
+//        if (loginResponse.data.userNotFound) {
+//          setNotFoundMessage('Esse usuário não existe. Por favor, registre um usuário que exista.');
+//          setErrorMessage('');
+//          return;
+//        }
+ 
+//        const productResponse = await axios.post(
+//          `http://localhost:3005/products`,
+//          {
+//            name: formData.nameProduct,
+//            description: formData.description,
+//            company: formData.company,
+//            price: formData.price,
+//            amount: formData.amount
+//          },
+//          {
+//            headers: {
+//              authorization: token
+//            }
+//          }
+//        );
+ 
+//        if (productResponse.data.message === 'Product created') {
+//          setInsert(true);
+//          setNotFoundMessage('');
+//          setErrorMessage('');
+//        }
+//      } catch (error) {
+//        if (error.response && error.response.status === 401) {
+//          setErrorMessage('Credenciais inválidas. Verifique seu e-mail e senha.');
+//          setNotFoundMessage('');
+//        } else {
+//          console.error('Erro ao enviar requisição:', error);
+//        }
+//      }
+//    };
+ 
+//    return (
+//      <main>
+//        <h1>Produtos</h1>
+//        <form onSubmit={handleSubmit}>
+//          <div className={styles.column}>
+//            <div>
+//              <label>E-mail</label>
+//            </div>
+//            <div>
+//              <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+//            </div>
+ 
+//            <div>
+//              <label>Senha</label>
+//            </div>
+//            <div>
+//              <input type="password" name="password" value={formData.password} onChange={handleChange} required />
+//            </div>
+//          </div>
+ 
+//          <div className={styles.column}>
+//            <div>
+//              <label>Nome do Produto</label>
+//            </div>
+//            <div>
+//              <input type="text" name="nameProduct" value={formData.nameProduct} onChange={handleChange} />
+//            </div>
+ 
+//            <div>
+//              <label>Descrição</label>
+//            </div>
+//            <div>
+//              <input type="text" name="description" value={formData.description} onChange={handleChange} />
+//            </div>
+ 
+//            <div>
+//              <label>Empresa</label>
+//            </div>
+//            <div>
+//              <input type="text" name="company" value={formData.company} onChange={handleChange} />
+//            </div>
+ 
+//            <div>
+//              <label>Preço</label>
+//            </div>
+//            <div>
+//              <input type="number" name="price" value={formData.price} onChange={handleChange} />
+//            </div>
+ 
+//            <div>
+//              <label>Quantidade</label>
+//            </div>
+//            <div>
+//              <input type="number" name="amount" value={formData.amount} onChange={handleChange} />
+//            </div>
+//          </div>
+ 
+//          {notFoundMessage && (
+//            <div className={styles.notFoundClass}>
+//              <p>{notFoundMessage}</p>
+//            </div>
+//          )}
+ 
+//          {errorMessage && (
+//            <div className={styles.error}>
+//              <p>{errorMessage}</p>
+//            </div>
+//          )}
+ 
+//          <div className={styles.lineButton}>
+//            <button type="submit">Enviar</button>
+//          </div>
+//        </form>
+//      </main>
+//    );
+//  }
+"use client"
+import React, { useState } from 'react';
+import axios from 'axios';
+import styles from './page.module.css';
 
 export default function Home() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    nameProduct: '',
+    description: '',
+    company: '',
+    price: '',
+    amount: ''
+  });
+
+  const [insert, setInsert] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [notFoundMessage, setNotFoundMessage] = useState('');
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const loginResponse = await axios.get(`http://localhost:3005/auth/login`, {
+        params: {
+          email: formData.email,
+          password: formData.password
+        }
+      });
+
+      if (loginResponse.data.userNotFound) {
+        setNotFoundMessage('Esse usuário não existe. Por favor, registre um usuário que exista.');
+        setErrorMessage('');
+        return;
+      }
+
+      const token = loginResponse.data.token;
+
+      const productResponse = await axios.post(
+        `http://localhost:3005/products`,
+        {
+          name: formData.nameProduct,
+          description: formData.description,
+          company: formData.company,
+          price: formData.price,
+          amount: formData.amount
+        },
+        {
+          headers: {
+            authorization: token
+          }
+        }
+      );
+
+      if (productResponse.data.message === 'Product created') {
+        setInsert(true);
+        setNotFoundMessage('');
+        setErrorMessage('');
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        setErrorMessage('Credenciais inválidas. Verifique seu e-mail e senha.');
+        setNotFoundMessage('');
+      } else {
+        console.error('Erro ao enviar requisição:', error);
+        setErrorMessage('Erro ao enviar requisição. Por favor, tente novamente.');
+        setNotFoundMessage('');
+      }
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main>
+      {insert ? (
+        <span>Inserção ocorreu com sucesso</span>
+      ) : (
+        <>
+          <h1>Produtos</h1>
+          <form onSubmit={handleSubmit}>
+            <div className={styles.column}>
+              <div>
+                <label>E-mail</label>
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+              <div>
+                <label>Senha</label>
+                <input
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+            <div className={styles.column}>
+              <div>
+                <label>Nome do Produto</label>
+                <input
+                  type="text"
+                  name="nameProduct"
+                  value={formData.nameProduct}
+                  onChange={handleChange}
+                />
+              </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+              <div>
+                <label>Descrição</label>
+                <input
+                  type="text"
+                  name="description"
+                  value={formData.description}
+                  onChange={handleChange}
+                />
+              </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+              <div>
+                <label>Empresa</label>
+                <input
+                  type="text"
+                  name="company"
+                  value={formData.company}
+                  onChange={handleChange}
+                />
+              </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+              <div>
+                <label>Preço</label>
+                <input
+                  type="number"
+                  name="price"
+                  value={formData.price}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div>
+                <label>Quantidade</label>
+                <input
+                  type="number"
+                  name="amount"
+                  value={formData.amount}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+
+            {notFoundMessage && (
+              <div className={styles.notFoundClass}>
+                <p>{notFoundMessage}</p>
+              </div>
+            )}
+
+            {errorMessage && (
+              <div className={styles.errorClass}>
+                <p>{errorMessage}</p>
+              </div>
+            )}
+
+            <div className={styles.lineButton}>
+              <button type="submit">Enviar</button>
+            </div>
+          </form>
+        </>
+      )}
     </main>
-  )
+  );
 }
